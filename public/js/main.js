a414a35e7c5d78abab79d62d1ed628ae9e5d9035
brysommermac <brysommer1@gmail.com>
2022-12-15 22:29:52 +0200

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$(".toggle-password").click(function() {
+
+	  $(this).toggleClass("fa-eye fa-eye-slash");
+	  var input = $($(this).attr("toggle"));
+	  if (input.attr("type") == "password") {
+	    input.attr("type", "text");
+	  } else {
+	    input.attr("type", "password");
+	  }
+	});
+
+})(jQuery);
+/*
+const passEyes = document.querySelector('.toggle-password');
+passEyes.addEventListener('click', (event) => {
+	passEyes.classList.toggle('fa-eye', 'fa-eye-slash');
+})
+*/
